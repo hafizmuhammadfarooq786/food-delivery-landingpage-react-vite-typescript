aac7e30cc1cbc9583181efd79e522872f16f0c4e
Muhammad Farooq <hafizmuhammadfarooq712@gmail.com>
2024-07-30 17:20:01 +0500

    created a layout design to interact with components and our App.tsx only responsible for loading data, setting data, and render layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,19 @@
+// App.tsx
 import React, { useEffect, useState } from "react";
-import { ButtonStatus, Text } from "./constants/appConstants";
-import { Colors } from "./constants/colorConstants";
-import Categories from "./components/categories";
-import ProductCard from "./components/common/card";
-import Button from "./components/common/button";
 import { CategoriesResponse, FoodResponse } from "./models/foodModels";
-import Search from "./components/search";
-import Empty from "./components/common/empty";
 import { getFoodCategories, getFoodList } from "./services/Api";
-import PlusIcon from "./assets/plus.svg";
-import Loader from "./components/common/loader";
+import Layout from "./layout";
 
 const App: React.FC = React.memo(() => {
   const [loading, setLoading] = useState<boolean>(false);
   const [searchFlag, setSearchFlag] = useState<boolean>(false);
-  const [categories, setCategories] = useState<any>([]);
-  const [foodItems, setFoodItems] = useState<any>([]);
-  const [trackItem, setTrackItems] = useState<any>([]);
-  const [visibleItems, setVisibleItems] = useState<any>([]);
+  const [categories, setCategories] = useState<any[]>([]);
+  const [foodItems, setFoodItems] = useState<any[]>([]);
+  const [trackItem, setTrackItems] = useState<any[]>([]);
+  const [visibleItems, setVisibleItems] = useState<any[]>([]);
   const [page, setPage] = useState<number>(1);
   const itemsPerPage = 9;
 
-  // Funations
-  // onClick => Show More Button
   const handleShowMore = () => {
     const nextPage = page + 1;
     const newVisibleItems = trackItem.slice(0, nextPage * itemsPerPage);
@@ -30,7 +21,6 @@ const App: React.FC = React.memo(() => {
     setPage(nextPage);
   };
 
-  // Function to handle category selection or search
   const handleChange = (items: FoodResponse[]) => {
     setTrackItems(items);
     setPage(1);
@@ -41,10 +31,7 @@ const App: React.FC = React.memo(() => {
     const fetchData = async () => {
       const categoryResults: CategoriesResponse[] = await getFoodCategories();
       if (categoryResults.length) {
-        categoryResults.unshift({
-          id: "",
-          name: "all",
-        });
+        categoryResults.unshift({ id: "", name: "all" });
         setCategories(categoryResults);
       }
       const foodResults: FoodResponse[] = await getFoodList();
@@ -57,57 +44,18 @@ const App: React.FC = React.memo(() => {
     fetchData();
   }, []);
 
-  return !loading ? (
-    <div className="container">
-      {/* Search */}
-      <Search
-        foodItems={foodItems}
-        setSearchFlag={setSearchFlag}
-        searchResults={(result: FoodResponse[]) => handleChange(result)}
-      />
-      {/* Categories */}
-      {categories.length && (
-        <Categories
-          categories={categories}
-          foodItems={foodItems}
-          searchFlag={searchFlag}
-          handleClick={(result: FoodResponse[]) => handleChange(result)}
-        />
-      )}
-
-      {/* GridBox containing product cards */}
-      <div className="gridBox">
-        {visibleItems.length ? (
-          visibleItems.map((source: FoodResponse, index: number) => (
-            <ProductCard key={index} {...source} />
-          ))
-        ) : (
-          <Empty />
-        )}
-      </div>
-
-      {/* Show More Button */}
-      {visibleItems.length < trackItem.length && (
-        <div className="row justifyCenter">
-          <Button
-            buttonText={Text.ShowMore}
-            buttonStatus={ButtonStatus.Active}
-            handleClick={handleShowMore}
-            icon={PlusIcon}
-            border={true}
-            borderRadius={12}
-            borderWidth={2}
-            borderStyle="solid"
-            borderColor={Colors.Yellow}
-          />
-        </div>
-      )}
-    </div>
-  ) : (
-    <div className="loading_screen">
-      <Loader color={Colors.Black} secondaryColor={Colors.Slate} />
-      <p>Loading, please wait...</p>
-    </div>
+  return (
+    <Layout
+      loading={loading}
+      searchFlag={searchFlag}
+      categories={categories}
+      foodItems={foodItems}
+      visibleItems={visibleItems}
+      trackItem={trackItem}
+      handleShowMore={handleShowMore}
+      handleChange={handleChange}
+      setSearchFlag={setSearchFlag}
+    />
   );
 });
 
